c7bd36c08a54c46e7e055787d59d869e469f7637
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2024-06-13 17:27:41 +0530

    redux example

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import {
-  createStore,
-  compose,
-  applyMiddleware,
-  bindActionCreators
-} from "redux";
-
-const makeLouderc = string => string.toUpperCase();
-const repeatThreeTimes = string => string.repeat(3);
-const embolden = string => string.bold();
-
-// const makeLoduderRepeatThreeTimesAndEmbolden = string => 
-  // embolden(repeatThreeTimes(makeLouderc(string)));
-
-
+// import {
+//   createStore,
+//   compose,
+//   applyMiddleware,
+//   bindActionCreators,
+// } from "redux";
 /*
+const makeLouderc = (string) => string.toUpperCase();
+const repeatThreeTimes = (string) => string.repeat(3);
+const embolden = (string) => string.bold();
+
+const makeLoduderRepeatThreeTimesAndEmbolden = string =>
+embolden(repeatThreeTimes(makeLouderc(string)));
+
+
 const makeLoduderRepeatThreeTimesAndEmbolden = compose(
   embolden,
   repeatThreeTimes,
@@ -24,10 +23,106 @@ const makeLoduderRepeatThreeTimesAndEmbolden = compose(
 console.log(makeLoduderRepeatThreeTimesAndEmbolden("hello"));
 */
 
+/****
+ *
+ * simplest reducer
+ *
+ */
+
+/*
 const initialState = {
-  value: 0
-}
+  value: 0,
+};
+
+const incrementAction = {
+  type: "INCREMENT",
+};
 
 const reducer = (state, action) => {
-  return state
-}
+  return state;
+};
+
+const store = createStore(reducer);
+
+console.log(store);
+*/
+
+/***
+ * use of a constant for the action type
+ *
+ */
+
+/*
+const initialState = {
+  value: 0,
+};
+
+const INCREMENT = "INCREMENT";
+
+const incrementAction = {
+  type: INCREMENT,
+};
+
+const reducer = (state, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      value: state.value + 1,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(reducer);
+
+console.log(store);
+*/
+
+
+/****
+ * 
+ * Practical implementation of a reducer, action creator and a dispatch
+ * 
+ */
+/*
+import { createStore } from "redux";
+
+const initialState = {
+  value: 0,
+};
+
+const INCREMENT = "INCREMENT";
+const ADD = "ADD";
+
+const incremenetAction = {
+  type: INCREMENT,
+};
+
+const increment = () => ({ type: INCREMENT });
+const add = (amount) => ({ type: ADD, payload: amount });
+
+const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      value: state.value + 1,
+    };
+  }
+  if (action.type === ADD) {
+    return {
+      value: state.value + action.payload,
+    };
+  }
+  return state;
+};
+
+const store = createStore(reducer);
+console.log(store);
+
+store.dispatch(increment());
+console.log(store.dispatch(increment()));
+
+console.log(store.getState());
+
+console.log(store.dispatch(add(5)));
+console.log(store.getState());
+*/
